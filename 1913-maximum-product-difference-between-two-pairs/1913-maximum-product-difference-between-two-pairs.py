beffaaf017d7f9858d67048f1ec9ffcbb5586d77
satyam6231631-hue <satyam6231631@gmail.com>
2026-09-07 11:16:11 +0530

    Time: 256 ms (5.27%), Space: 20.6 MB (76.58%) - LeetHub

---
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        def part(nums,lo,hi):
+            pivot,pos=nums[hi],lo-1
+            for i in range(lo,hi+1):
+                if nums[i]<=pivot:
+                    pos+=1
+                    nums[i],nums[pos]=nums[pos],nums[i]
+            return pos
+        def qs(nums,lo,hi):
+            if lo>=hi:
+                return
+            idx=part(nums,lo,hi)
+            qs(nums,lo,idx-1)
+            qs(nums,idx+1,hi)
+        qs(nums,0,len(nums)-1)
+        return (nums[-1]*nums[-2])-(nums[0]*nums[1])
+        
